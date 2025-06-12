cf3f02eabf979eff3a53a93938285ab1d3e1a11f
caneno <nook.of.little.things@gmail.com>
2025-06-12 16:57:49 -0600

    CLI is able to create directories but still need some fixing

--- go-cli/cli.go
+++ go-cli/cli.go
@@ -1,28 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
-var workingDir string
+// Config holds the values parsed from command-line flags.
+type Config struct {
+	pname string
+	path  string
+}
 
-func init() {
-	var err error
-	workingDir, err = os.Getwd()
+// parserFlag initializes and returns a Config struct with parsed flag values.
+
+func parserFlag() Config {
+	now := time.Now().UTC()
+	cwd, err := os.Getwd()
 	if err != nil {
-		fmt.Println("Error getting working directory:", err)
-		os.Exit(1)
+		fmt.Println("Error getting current working directory:", err)
 	}
+	var cfg Config
+
+	flag.StringVar(&cfg.pname, "pname", now.Format("20060102")+"_NewProject", "Name of the Project/parent directory")
+	flag.StringVar(&cfg.path, "path", cwd, "Path of where the project is going to live.")
+	flag.Parse()
+
+	return cfg
 }
 
-func treeFolder() {
-
-}
+// main initializes configuration and passes it to other functions.
 
 func main() {
+	cfg := parserFlag()
+	testfun(cfg)
+}
 
-	// err := os.Mkdir()
-	fmt.Println(workingDir)
+// testfun uses the Config values to print project details.
 
+func testfun(cfg Config) {
+	directories := []string{"src", "tests", "infra", "ci", "scripts", "configs", "docs"}
+	for _, direct := range directories {
+		err := os.Mkdir(cfg.path+"\\"+direct, 0755)
+		if err != nil {
+			fmt.Printf("Failed to create folder %s: %v\n", direct, err)
+		}
+	}
 }
