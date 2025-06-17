7697eaead9f6b4ae34c350efb23063efe656c7b6
Ray <ray.moon89@gmail.com>
2025-06-16 20:26:26 -0700

    change gitignore and folder creation

--- go-cli/cli.go
+++ go-cli/cli.go
@@ -17,14 +17,10 @@ type Config struct {
 
 func parserFlag() Config {
 	now := time.Now().UTC()
-	cwd, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error getting current working directory:", err)
-	}
 	var cfg Config
 
 	flag.StringVar(&cfg.pname, "pname", now.Format("20060102")+"_NewProject", "Name of the Project/parent directory")
-	flag.StringVar(&cfg.path, "path", cwd, "Path of where the project is going to live.")
+	flag.StringVar(&cfg.path, "path", "./", "Path of where the project is going to live.")
 	flag.Parse()
 
 	return cfg
@@ -40,9 +36,13 @@ func main() {
 // testfun uses the Config values to print project details.
 
 func testfun(cfg Config) {
-	directories := []string{"src", "tests", "infra", "ci", "scripts", "configs", "docs"}
-	for _, direct := range directories {
-		err := os.Mkdir(cfg.path+"\\"+direct, 0755)
+	childDir := []string{"src", "tests", "infra", "ci", "scripts", "configs", "docs"}
+	err := os.Mkdir(cfg.path+"/"+cfg.pname, 0755)
+	if err != nil {
+		fmt.Printf("Failed to create folder %v\n", err)
+	}
+	for _, direct := range childDir {
+		err := os.Mkdir(cfg.path+"/"+cfg.pname+"/"+direct, 0755)
 		if err != nil {
 			fmt.Printf("Failed to create folder %s: %v\n", direct, err)
 		}
